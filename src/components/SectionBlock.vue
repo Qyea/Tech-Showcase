795f0b867f46ba9719fb31cfee7abd2d1c529830
Anastasia Krech <kreshess@mail.ru>
2024-07-16 23:18:36 +0300

    Profile Block

--- src/components/SectionBlock.vue
+++ src/components/SectionBlock.vue
@@ -6,46 +6,35 @@
     <div class="wrap">
       <div class="items-wrap">
         <div class="items marquee">
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
+          <div
+            v-for="_ in blockData.title.split('').length > 10 ? 5 : 9"
+            class="running-title"
+          >
+            {{ blockData.title }}
+          </div>
         </div>
         <div aria-hidden="true" class="items marquee">
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
-          <div class="running-title">{{ blockData.title }}</div>
+          <div
+            v-for="_ in blockData.title.split('').length > 10 ? 5 : 9"
+            class="running-title"
+          >
+            {{ blockData.title }}
+          </div>
         </div>
       </div>
     </div>
     <div class="block-wrapper">
       <h2 class="title">{{ blockData.title }}</h2>
-      <div class="info-block">
-        <div class="block-text">
+      <TextBlock image="./src/assets/images/profile-photo.jpg">
+        <template #text>
           I am a student of the Belarusian State University, Faculty of Applied
           Mathematics and Computer Science. In three years of study, I gained
           basic knowledge of both low-level languages, such as assembler, and
           high-level programming languages: C++, Java.
-        </div>
-        <img
-          class="image-circle"
-          src="./assets/profile-photo.jpg"
-          alt="profile-photo"
-        />
-      </div>
-      <div class="info-block">
-        <div class="languages">
+        </template>
+      </TextBlock>
+      <TextBlock :table="true">
+        <template #table>
           <div class="centered-text">I worked with the following languages</div>
           <ul class="list">
             <li>JavaScript</li>
@@ -54,22 +43,22 @@
             <li>Python</li>
             <li>SQL</li>
           </ul>
-        </div>
-        <div class="block-text">
+        </template>
+        <template #text>
           My experience includes writing client-server applications, desktop
           applications, which included working with databases and creating user
           interfaces. I also have experience in solving basic algorithmic
           problems.
-        </div>
-      </div>
+        </template>
+      </TextBlock>
     </div>
   </section>
-  <v-slot></v-slot>
+  <slot name="text"></slot>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-
+import TextBlock from "./TextBlock.vue";
 //defineProps<{ msg: string }>();
 
 const blockData = {
@@ -130,5 +119,14 @@ const count = ref(0);
   to {
     transform: translateX(calc(-100% - 20px));
   }
+}
+
+.block-wrapper {
+  margin: 0 40px 40px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/TextBlock.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="info-block">
+    <div v-if="props.table" class="languages">
+      <slot name="table"></slot>
+    </div>
+    <div class="block-text"><slot name="text"> </slot></div>
+    <div v-if="props.image">
+      <img class="image-circle" v-bind:src="props.image" alt="profile-photo" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  image?: string;
+  table?: boolean;
+}>();
+
+console.log(props.image);
+</script>
+
+<style lang="scss" scoped>
+.info-block {
+  display: flex;
+  justify-content: center;
+  gap: 3.5rem;
+  align-items: center;
+
+  margin-bottom: 2rem;
+}
+
+.block-text {
+  width: 55%;
+  word-wrap: break-word;
+  text-align: justify;
+  line-height: 120%;
+}
+
+.image-circle {
+  position: relative;
+  object-fit: cover;
+
+  width: var(--cv-component-size);
+  height: var(--cv-component-size);
+  //   border: 0.5rem solid var(--cv-translucent-white);
+  outline-offset: -0.5rem;
+  outline: 0.5rem solid rgba(255, 255, 255, 0.3);
+  border-radius: 50%;
+
+  @media screen and (max-width: 600px) {
+    display: none;
+  }
+}
+
+.languages {
+  width: 20%;
+  padding: 1.5rem 1.25rem 0;
+
+  display: flex;
+  flex-direction: column;
+
+  font-size: var(--cv-font-size-ms);
+  font-style: italic;
+  text-align: center;
+
+  border: 0.2rem solid var(--cv-translucent-white);
+
+  @media screen and (max-width: 600px) {
+    padding: 0;
+    font-size: var(--cv-mobile-font-size-ms);
+  }
+}
+</style>
